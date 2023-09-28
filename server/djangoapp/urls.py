4ac1833c523ca72346018afc109e7167a9c36490
jbaspra <jbaspra@gmail.com>
2023-09-28 12:29:47 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,19 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about', view=views.about,name='about'),
+    #path(route='about', view=views.about,name='about'),
 
     # path for contact us view
-    path(route='contact', view=views.contact,name='contact'),
+    #path(route='contact', view=views.contact,name='contact'),
 
     # path for registration
-    path(route='registration', view=views.contact,name='registration'),
+    path(route='registration', view=views.registration_request,name='registration'),
 
     # path for login
-    path(route='login', view=views.contact,name='login'),
+    path(route='login', view=views.login,name='login'),
 
     # path for logout
-    path(route='logout', view=views.contact,name='logout'),
+    path(route='logout', view=views.logout,name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
